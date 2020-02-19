9e95762c73ea6eacdaed6205b930a5d92f8e816b
Erwin Kooi <erwin.kooi@empire.ca>
2020-02-19 15:52:55 -0500

    postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,9 @@
 import os
-
+from dotenv import load_dotenv
 # Django settings for mysite project.
+import dj_database_url
+
+load_dotenv('.env')
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -20,15 +23,7 @@ PHRASE_SUFFIX = '__}}'
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL', 'postgres://postgres@localhost/demo'))
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
